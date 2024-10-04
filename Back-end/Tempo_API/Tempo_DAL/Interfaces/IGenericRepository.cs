b0692fabcaa1b9be65a63932782d51d89109c13c
anast <anastasialukyanova27@gmail.com>
2024-10-04 15:36:23 +0300

    Add Generic repository

---
+++ Back-end/Tempo_API/Tempo_DAL/Interfaces/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Tempo_DAL.Entities;
+
+namespace Tempo_DAL.Interfaces;
+
+public interface IGenericRepository<Entity> where Entity : BaseEntity
+{
+    Task<Entity> Create(Entity element, CancellationToken cancellationToken);
+    Task Delete(Guid id, CancellationToken cancellationToken);
+    Task<Entity> Update(Entity element, CancellationToken cancellationToken);
+    Task<List<Entity>> GetAll(CancellationToken cancellationToken);
+    Task<Entity?> GetById(Guid id, CancellationToken cancellationToken);
+    Task<List<Entity>> GetByPredicate(Expression<Func<Entity, bool>> predicate, CancellationToken cancellationToken);
+}
---
+++ Back-end/Tempo_API/Tempo_DAL/Repositories/GenericRepository.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using Tempo_DAL.Entities;
+using Tempo_DAL.Interfaces;
+
+namespace Tempo_DAL.Repositories;
+
+public class GenericRepository<Entity> : IGenericRepository<Entity> where Entity : BaseEntity
+{
+    private readonly TempoDbContext dbContext;
+    private readonly DbSet<Entity> dbSet;
+
+    public GenericRepository(TempoDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+        dbSet = dbContext.Set<Entity>();
+    }
+
+    public async Task<Entity> Create(Entity element, CancellationToken cancellationToken)
+    {
+        dbSet.Add(element);
+        await dbContext.SaveChangesAsync(cancellationToken);
+        return element;
+    }
+
+    public async Task Delete(Guid id, CancellationToken cancellationToken)
+    {
+        var element = await dbSet.FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+
+        if (element != null)   
+        {
+            dbSet.Remove(element);
+            await dbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+
+    public async Task<Entity> Update(Entity element, CancellationToken cancellationToken)
+    {
+        dbSet.Update(element);
+        await dbContext.SaveChangesAsync(cancellationToken);
+        return element;
+    }
+
+    public Task<List<Entity>> GetAll(CancellationToken cancellationToken)
+    {
+        return dbSet.ToListAsync(cancellationToken);
+    }
+
+    public Task<Entity?> GetById(Guid id, CancellationToken cancellationToken)
+    {
+        return dbSet.AsNoTracking().Where(x => x.Id == id).FirstOrDefaultAsync(cancellationToken);
+    }
+
+    public Task<List<Entity>> GetByPredicate(Expression<Func<Entity, bool>> predicate, CancellationToken cancellationToken)
+    {
+        return dbSet.AsNoTracking().Where(predicate).ToListAsync(cancellationToken);
+    }
+
+
+}
