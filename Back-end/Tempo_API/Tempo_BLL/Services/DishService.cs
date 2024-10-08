801d7290ef810765226da162af855306ff0ce60e
anast <anastasialukyanova27@gmail.com>
2024-10-08 23:02:50 +0300

    Add Services

---
+++ Back-end/Tempo_API/Tempo_BLL/Services/DishService.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Tempo_BLL.Interfaces;
+using Tempo_BLL.Models;
+using Tempo_DAL.Entities;
+using Tempo_DAL.Interfaces;
+
+namespace Tempo_BLL.Services;
+
+public class DishService : GenericService<DishModel, DishEntity>, IDishService
+{
+    public DishService(IMapper mapper, IDishRepository repository) : base(mapper, repository) 
+    {
+    }
+}
---
+++ Back-end/Tempo_API/Tempo_BLL/Services/DrinkService.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Tempo_BLL.Interfaces;
+using Tempo_BLL.Models;
+using Tempo_DAL.Entities;
+using Tempo_DAL.Interfaces;
+
+namespace Tempo_BLL.Services;
+
+public class DrinkService : GenericService<DrinkModel, DrinkEntity>, IDrinkService
+{
+    public DrinkService(IMapper mapper, IDrinkRepository repository) : base(mapper, repository) 
+    {
+    }
+}
