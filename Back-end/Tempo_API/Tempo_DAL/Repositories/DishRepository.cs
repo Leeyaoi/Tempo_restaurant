91ac4d33ad7545388a5903b9f55290b11f8a04ed
anast <anastasialukyanova27@gmail.com>
2024-10-08 15:22:52 +0300

    Add-Repositories

---
+++ Back-end/Tempo_API/Tempo_DAL/Repositories/DishRepository.cs
@@ -0,0 +1,11 @@
+﻿using Tempo_DAL.Entities;
+using Tempo_DAL.Interfaces;
+
+namespace Tempo_DAL.Repositories;
+
+public class DishRepository : GenericRepository<DishEntity>, IDishRepository
+{
+    public DishRepository(TempoDbContext dbContext) : base(dbContext)
+    {
+    }
+}
