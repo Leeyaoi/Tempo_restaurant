39094fa0176880e595cc9109ce857f96b9144b7a
Leeyaoi <darya.ya2608@gmail.com>
2024-10-20 08:47:15 +0300

    added auth logic

--- Back-end/Tempo_API/Tempo_BLL/Services/UserService.cs
+++ Back-end/Tempo_API/Tempo_BLL/Services/UserService.cs
@@ -11,4 +11,14 @@ public class UserService : GenericService<UserModel, UserEntity>, IUserService
     public UserService(IMapper mapper, IUserRepository repository) : base(mapper, repository)
     {
     }
+
+    public override async Task<UserModel> Create(UserModel model, CancellationToken cancellationToken)
+    {
+        var search = await _repository.GetByPredicate(x => x.Name == model.Name && x.Phone == model.Phone, cancellationToken);
+        if (search.Count == 0)
+        {
+            return await base.Create(model, cancellationToken);
+        }
+        return _mapper.Map<UserModel>(search[0]);
+    }
 }
