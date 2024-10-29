1b1ac98e450d774aed90e9a8b7274a2ac43dcfbc
anast <anastasialukyanova27@gmail.com>
2024-10-29 23:36:15 +0300

    Add main Admin Page

--- Front-end/tempo_workers/src/App.tsx
+++ Front-end/tempo_workers/src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/loginPage/LoginPage";
+import MainAdminPage from "./pages/mainAdminPage/MainAdminPage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+      </Routes>
+      <Routes>
+        <Route path="/AdminPage" element={<MainAdminPage />} />
       </Routes>
     </BrowserRouter>
   );
